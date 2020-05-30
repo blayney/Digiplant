47323cebeab8fcab9b340a8f5149e499ed3ba446
Will Blayney <woblayney@gmail.com>
2020-05-30 20:39:58 +0100

    Fixed Tbl

--- Digiplant2/Digiplant2/ViewController.swift
+++ Digiplant2/Digiplant2/ViewController.swift
@@ -8,13 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
 
+    var Plants = ["Item1", "Item2"]
+
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return.lightContent
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return Plants.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FreddieRawlins")!
+        let text = Plants[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
     }
 
-
+    
+    @IBAction func TappedAddPlant() {
+        
+        let alert = UIAlertController(title: "Add Plant", message: nil, preferredStyle: .alert)
+        alert.addTextField { (UITextField) in
+            UITextField.placeholder = "Enter Plant Name"
+        }
+        
+        let action = UIAlertAction(title:"Add", style: .default) {
+            (_) in guard let plant = alert.textFields?.first?.text else {return}
+            print(plant)
+            self.addPlant(plant)
+        }
+        alert.addAction(action)
+        present(alert,animated: true)
+        
+    }
+    
+    func addPlant(_ plant: String){
+        let index = 0
+        Plants.insert(plant, at: index)
+        
+        tableView.beginUpdates()
+        tableView.insertRows(at: [IndexPath(row: Plants.count - 1, section: 0)], with: .automatic)
+        tableView.endUpdates()
+        }
 }
+
 

15b124ab798c96cd373a8d5a459de99244df8df6
Will Blayney <woblayney@gmail.com>
2020-05-30 17:07:05 +0100

    Initial XCode

---
+++ Digiplant/Digiplant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Digiplant
+//
+//  Created by Will Blayney on 30/05/2020.
+//  Copyright © 2020 Will Blayney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Test, Digiplant")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

63d75a181b5baea78c76b75034133275961d896a
Will Blayney <woblayney@gmail.com>
2020-05-31 09:05:07 +0100

    Addded VAR

---
+++ Digiplant2/Digiplant2/SetTypeVC.swift
@@ -0,0 +1,35 @@
+//
+//  SetTypeVC.swift
+//  Digiplant2
+//
+//  Created by Will Blayney on 31/05/2020.
+//  Copyright © 2020 Will Blayney. All rights reserved.
+//
+
+import UIKit
+
+class SetTypeVC: UIViewController {
+
+    
+    var SearchResults = [String]()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

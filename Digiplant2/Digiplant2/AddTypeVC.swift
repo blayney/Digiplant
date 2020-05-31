bee1c2f36b23f9956d88cd06b59ea1f0b79cc0a4
Will Blayney <woblayney@gmail.com>
2020-05-31 12:40:00 +0100

    Final Committy

---
+++ Digiplant2/Digiplant2/AddTypeVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddTypeVC.swift
+//  Digiplant2
+//
+//  Created by Will Blayney on 31/05/2020.
+//  Copyright © 2020 Will Blayney. All rights reserved.
+//
+
+import UIKit
+
+class AddTypeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

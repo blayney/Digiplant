9fa0d34234815e2c1ec3b3570746efc498ee408e
Hugh <Hugh.blayney@gmail.com>
2020-05-31 12:40:08 +0100

    Added api and local backend

---
+++ Digiplant2/Digiplant2/LocalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocalViewController.swift
+//  Digiplant2
+//
+//  Created by Hugh Blayney on 31/05/2020.
+//  Copyright © 2020 Will Blayney. All rights reserved.
+//
+
+import UIKit
+
+class LocalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Digiplant2/Digiplant2/global.swift
@@ -0,0 +1,9 @@
+//
+//  global.swift
+//  Digiplant2
+//
+//  Created by Hugh Blayney on 31/05/2020.
+//  Copyright © 2020 Will Blayney. All rights reserved.
+//
+
+import Foundation
